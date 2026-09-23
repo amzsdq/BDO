b79359451e0a76529e9f71405e63d2fe0737152c
amzsdq <74319254+amzsdq@users.noreply.github.com>
2026-09-23 19:22:17 +0900

    feat: gate planner mount behind dataset-first hydration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import { PlannerBootstrapBoundary } from './PlannerBootstrapBoundary'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PlannerBootstrapBoundary>{() => <App />}</PlannerBootstrapBoundary>
   </StrictMode>,
 )
