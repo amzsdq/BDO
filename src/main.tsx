ebd5cf82678c7ae78ee10f129c310b775a383418
amzsdq <74319254+amzsdq@users.noreply.github.com>
2026-09-24 02:01:16 +0900

    Remove duplicate global planner state actions (#140)

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import { PlannerBootstrapBoundary } from './PlannerBootstrapBoundary'
-import { PlannerStateActions } from './PlannerStateActions'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PlannerBootstrapBoundary>{(bootstrap) => <>
-      <App bootstrap={bootstrap} />
-      <PlannerStateActions onReset={() => window.location.reload()} />
-    </>}</PlannerBootstrapBoundary>
+    <PlannerBootstrapBoundary>{(bootstrap) => <App bootstrap={bootstrap} />}</PlannerBootstrapBoundary>
   </StrictMode>,
 )
